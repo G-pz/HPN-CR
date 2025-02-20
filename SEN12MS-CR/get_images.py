98a2776fe9be784ec92d9fe4c7f016fee98d3a50
Yixuan <73058962+G-pz@users.noreply.github.com>
2025-02-20 10:28:49 +0800

    Update get_images.py

--- SEN12MS-CR/get_images.py
+++ SEN12MS-CR/get_images.py
@@ -6,18 +6,18 @@ import shutil
 # The target folder will contain all image files from the source folder
 
 #Target folder path
-determination = ['./SEN12MS-CR/train/s1/', './SEN12MS-CR/train/s2_cloudfree/', './SEN12MS-CR/train/s2_cloudy/', 
+_determination_ = ['./SEN12MS-CR/train/s1/', './SEN12MS-CR/train/s2_cloudfree/', './SEN12MS-CR/train/s2_cloudy/', 
                  './SEN12MS-CR/val/s1/', './SEN12MS-CR/val/s2_cloudfree/', './SEN12MS-CR/val/s2_cloudy/',
                  './SEN12MS-CR/test/s1/', './SEN12MS-CR/test/s2_cloudfree/', './SEN12MS-CR/test/s2_cloudy/'] 
 
 #Source folder path
-path = ['./SEN12MS-CR/train/source_folder/s1/', './SEN12MS-CR/train/source_folder/s2/', './SEN12MS-CR/train/source_folder/s2_cloudy/', 
+_path_ = ['./SEN12MS-CR/train/source_folder/s1/', './SEN12MS-CR/train/source_folder/s2/', './SEN12MS-CR/train/source_folder/s2_cloudy/', 
         './SEN12MS-CR/val/source_folder/s1/', './SEN12MS-CR/val/source_folder/s2/', './SEN12MS-CR/val/source_folder/s2_cloudy/', 
         './SEN12MS-CR/test/source_folder/s1/', './SEN12MS-CR/test/source_folder/s2/', './SEN12MS-CR/test/source_folder/s2_cloudy/']
 
-for i in range(len(determination)):
-    determination = determination[i]
-    path = path[i]
+for i in range(len(_determination_)):
+    determination = _determination_[i]
+    path = _path_[i]
     
     if not os.path.exists(determination):
         os.makedirs(determination)
